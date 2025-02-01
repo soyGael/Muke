cc06e50fd583c9cb24451096ad23d7f31d87c04f
soy Gael <gutierrezgael838@gmail.com>
2025-01-31 22:58:31 -0500

    The header is created

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <>
+      <header className="uk-tile uk-padding-small uk-flex uk-flex-between uk-flex-middle uk-box-shadow-medium">
+        <div className="">
+          <h1 className="uk-text-bold uk-margin-xsmall">Muke</h1>
+        </div>
+        <div className="uk-margin-xsmall uk-flex uk-flex-column uk-flex-right">
+          <p className="uk-margin-xsmall">Wednesday, 31</p>
+          <p className="uk-margin-xsmall">Jan, 25</p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
