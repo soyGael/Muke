e6cab477d5dbf62afda98af906ba4ad2113ebad1
soy Gael <gutierrezgael838@gmail.com>
2025-02-01 14:48:45 -0500

    It's add the filters

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 const Tasks = ({ newTask, onEditTask, onDeleteTask }) => {
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState('Todas');
 
   useEffect(() => {
     const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -28,20 +29,38 @@ const Tasks = ({ newTask, onEditTask, onDeleteTask }) => {
     onEditTask(taskToEdit, index);
   };
 
+  const filteredTasks = tasks.filter(task => {
+    if (filter === 'Pendientes') {
+      return task.status === 'Pendiente';
+    } else if (filter === 'Terminadas') {
+      return task.status === 'Terminado';
+    } else {
+      return true;
+    }
+  });
+
   return (
     <div className="uk-card uk-card-default uk-card-body">
       <h2 className='uk-card-title'>Lista de Tareas</h2>
-      {tasks.length === 0 ? (
+      <div className="uk-margin">
+        <label htmlFor="filter" className="uk-form-label">Filtrar por:</label>
+        <select id="filter" className="uk-select" value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <option value="Todas">Todas</option>
+          <option value="Pendientes">Pendientes</option>
+          <option value="Terminadas">Terminadas</option>
+        </select>
+      </div>
+      {filteredTasks.length === 0 ? (
         <p>No hay tareas disponibles. ¡Agrega una nueva tarea!</p>
       ) : (
         <ul>
-          {tasks.map((task, index) => (
+          {filteredTasks.map((task, index) => (
             <li key={index}>
               <h3>{task.title}</h3>
               <p>{task.description}</p>
               <p>Estado: {task.status}</p>
               <p>Fecha de creación: {task.createdAt}</p>
-              <button onClick={() => handleEdit} className="uk-button uk-button-primary">Editar</button>
+              <button onClick={() => handleEdit(index)} className="uk-button uk-button-primary">Editar</button>
               <button onClick={() => handleDelete(index)} className="uk-button uk-button-danger">Eliminar</button>
             </li>
           ))}
